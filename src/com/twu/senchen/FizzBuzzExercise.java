5e4c0eb3e48fc89765a6b7813fc5b90d79c03fd2
chensen <senchen@thoughtworks.com>
2018-08-31 10:14:24 +0800

    complete fizzbuzz exercises

--- src/com/twu/senchen/FizzBuzzExercise.java
+++ src/com/twu/senchen/FizzBuzzExercise.java
@@ -1,4 +1,24 @@
 package com.twu.senchen;
 
 public class FizzBuzzExercise {
+    public static void main(String[] args) {
+        int num = 100;
+        fizzbuzz(num);
+    }
+
+    private static void fizzbuzz(int num) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            } else {
+                if (i % 3 == 0) {
+                    System.out.print("Fizz");
+                }
+                if (i % 5 == 0) {
+                    System.out.print("Buzz");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
